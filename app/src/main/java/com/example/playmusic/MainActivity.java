77e86f6f1c077733da3592b5bc3b2d85254e1b4f
gildairmoreira <104792309+gildairmoreira@users.noreply.github.com>
2024-09-17 09:19:46 -0300

    feat: App terminado sem utilizar LifeCycle

--- app/src/main/java/com/example/playmusic/MainActivity.java
+++ app/src/main/java/com/example/playmusic/MainActivity.java
@@ -1,24 +1,47 @@
 package com.example.playmusic;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        ImageView pause = findViewById(R.id.pause);
+        ImageView play = findViewById(R.id.play);
+        ImageView stop = findViewById(R.id.stop);
+        mediaPlayer = MediaPlayer.create(this, R.raw.global);
+
+        pause.setOnClickListener(v -> {
+            if (mediaPlayer.isPlaying()) {
+                mediaPlayer.pause();
+            }
+        });
+
+        play.setOnClickListener(v -> {
+            if (mediaPlayer != null) {
+                mediaPlayer.start();
+            }
+
+        });
+
+        stop.setOnClickListener(v -> {
+            if (mediaPlayer.isPlaying()) {
+                mediaPlayer.stop();
+                mediaPlayer.release();
+                mediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.global);
+            }
+
         });
     }
 }
